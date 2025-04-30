2f4474e234e6d822e9b9ff2112c514d0f8cb8000
Tanisha780 <141040330+Tanisha780@users.noreply.github.com>
2025-04-30 19:46:57 +0530

    intersection-using-hashing

--- HashingQ.java
+++ HashingQ.java
@@ -1,21 +1,28 @@
 import java.util.*;
 class HashingQ{
-  // union of two arrays having unique values in new set
-  static int union(int[]nums1,int[]nums2){
-    HashSet<Integer>set = new HashSet<>();
+  // intersection of two arrays having unique values in new set
+  static int intersection(int[]nums1,int[]nums2){
+    HashSet<Integer> set = new HashSet<>();
+    int count=0;
     for(int i =0;i<nums1.length;i++){
       set.add(nums1[i]);
+
     }
-    for(int  j= 0;j<nums2.length;j++){
-      set.add(nums2[j]);
+    for(int j  = 0;j<nums2.length;j++){
+      if(set.contains(nums2[j]))
+      {
+        count++;
+        set.remove(nums2[j]);
+
+      }
     }
-    return set.size();
+    return count;
 
   }
 
   public static void main(String[] args) {
     int []nums1 ={1,2,3};
-    int[]nums2 = {4,5,6,1};
-    System.out.println(union(nums1, nums2));
+    int[]nums2 = {4,5,6,1,2};
+    System.out.println(intersection(nums1, nums2));
   }
 }
