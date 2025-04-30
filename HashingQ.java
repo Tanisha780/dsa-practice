c630cd769414f9a7750da3fe45660f14c3c1238e
Tanisha780 <141040330+Tanisha780@users.noreply.github.com>
2025-04-30 19:32:37 +0530

    union of two arrays

--- HashingQ.java
+++ HashingQ.java
@@ -1,31 +1,21 @@
 import java.util.*;
-//majority element
-// given an integer array of size n, find all elements that appear more than n/3 times
-
-class HashingQ {
-  static void majorityElement(int[]nums){
-    HashMap<Integer, Integer> map = new HashMap<>();
-    int n = nums.length;
-    for(int i = 0; i<n;i++){
-      // if key exists then updates the frequency value
-      if(map.containsKey(nums[i])){
-        map.put(nums[i],map.get(nums[i])+1);
-      }
-      // new pair else
-      else{
-        map.put(nums[i],1);
-      }
-
+class HashingQ{
+  // union of two arrays having unique values in new set
+  static int union(int[]nums1,int[]nums2){
+    HashSet<Integer>set = new HashSet<>();
+    for(int i =0;i<nums1.length;i++){
+      set.add(nums1[i]);
     }
-    for(int key : map.keySet()){
-      if(map.get(key)>n/3){
-        System.out.println(key);
-      }
+    for(int  j= 0;j<nums2.length;j++){
+      set.add(nums2[j]);
     }
+    return set.size();
+
   }
+
   public static void main(String[] args) {
-    int nums[]= {1,3};
-    majorityElement(nums);
+    int []nums1 ={1,2,3};
+    int[]nums2 = {4,5,6,1};
+    System.out.println(union(nums1, nums2));
   }
-  
 }
